6fed5502a439f393df99f89ae9b1ae1763e8a74d
dymafr <erwan1309@hotmail.com>
2022-03-22 21:54:29 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import TheHeader from './components/Header.vue';
+import TheFooter from './components/Footer.vue';
+import Boutique from './features/boutique/Boutique.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <TheHeader class="header" />
+    <div class="app-content"><Component :is="Boutique" /></div>
+    <TheFooter class="footer" />
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/scss/base.scss';
+@import './assets/scss/debug.scss';
+
+.app-container {
+  min-height: 100vh;
+  display: grid;
+  grid-template-areas: 'header' 'app-content' 'footer';
+  grid-template-rows: 48px auto 48px;
+}
+
+.header {
+  grid-area: header;
+}
+
+.app-content {
+  grid-area: app-content;
+}
+
+.footer {
+  grid-area: footer;
+}
+</style>
